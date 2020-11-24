091da49c67e9b9bac375c2842f5e0a33023ccd18
gumiku01 <zhanyuan239@gmail.com>
2020-11-25 00:33:32 +0100

    complete maximum sublist

--- maximum_subarray.py
+++ maximum_subarray.py
@@ -2,25 +2,28 @@
 # output: the sublist which has maximum sum of elements
 
 
-def compute_max(num_list, low, high, middle):
+def compute_cross_max(num_list, low, high, middle):
     # compute the left side
     left_max_sum = float('-inf')
     left_index = middle - 1
     left_sum = 0
-    for left in range(middle, low - 1, -1):
+    for left in range(middle - 1, low - 1, -1):
         left_sum += num_list[left]
         if left_sum > left_max_sum:
             left_max_sum = left_sum
             left_index = left
 
-    # compute the 
+    # compute the right side
     right_max_sum = float('-inf')
     right_index = middle
     right_sum = 0
     for right in range(middle, high):
         right_sum += num_list[right]
+        if right_sum > right_max_sum:
+            right_max_sum = right_sum
+            right_index = right
 
-
+    return left_index, right_index, left_max_sum + right_max_sum
 
 
 def maximum_sublist(num_list, low, high):
@@ -35,11 +38,20 @@ def maximum_sublist(num_list, low, high):
         middle = (low + high) // 2
         left_low_index, left_high_index, left_max = maximum_sublist(num_list, low, middle)
         right_low_index, right_high_index, right_max = maximum_sublist(num_list, middle, high)
-
-
-    return 1, 2, 3
+        cross_low_index, cross_high_index, cross_max = compute_cross_max(num_list, low, high, middle)
+        print(cross_max)
+        if left_max >= right_max and left_max >= cross_max:
+            return left_low_index, left_high_index, left_max
+        elif right_max >= left_max and right_max >= cross_max:
+            return right_low_index, right_high_index, right_max
+        else:
+            return cross_low_index, cross_high_index, cross_max
+    # otherwise only one element in list
+    return low, high, num_list[low]
 
 
 if __name__ == '__main__':
     test_case = [5, 6, -8, 2, 4, -9, 8, 7, -4, 1, 3, 5, 8, -10]
-
+    left_index, right_index, max_sum = maximum_sublist(test_case, 0, len(test_case))
+    print("the resutl: the max sum is %d from index %d to %d -> " % (max_sum, left_index, right_index),
+          test_case[left_index: right_index+1])
